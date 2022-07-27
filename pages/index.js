18a9482afe214ffa6de891b316b3a5b897ff1918
Mirosław Pragłowski <m@praglowski.com>
2022-07-27 19:41:41 +0200

    Add contact email icon

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,14 @@ import {
   faInstagram,
   faPinterest,
 } from "@fortawesome/free-brands-svg-icons";
+import {
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
 
 const socials = [
   {id: "instagram", url: "https://www.instagram.com/interior_smartdesign", icon: faInstagram, description: "Realizacje i projekty"},
   {id: "pinterest", url: "https://pl.pinterest.com/ewa_swiderska", icon: faPinterest, description: "Inspiracje"},
+  {id: "email", url: "mailto:ewa@warzynska.pl", icon: faEnvelope, description: "Kontakt"},
 ]
 
 export default function Home() {
@@ -25,14 +29,14 @@ export default function Home() {
             <img src="/profile.jpg"
                  className="rounded-full w-32 h-32 dark:opacity-75 shadow-xl"
             />
-            <h1 className="text-xl font-bold text-center">
+            <h1 className="text-2xl font-bold text-center">
               Ewa Warzyńska smartDesign
             </h1>
           </div>
           <ul className="flex justify-center items-center text-6xl mt-4 pt-4 border-t border-gold-800 space-x-8">
             { socials.map(social =>
               <li key={social.id}>
-                <a href={social.url}>
+                <a href={social.url} alt={social.description}>
                   <FontAwesomeIcon icon={social.icon} />
                 </a>
               </li>
