761484e12260c289530d6bc727de1aec38a2babc
Mirosław Pragłowski <m@praglowski.com>
2022-07-27 15:16:59 +0200

    Fix name

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2 text-gold-100 bg-background-900">
       <Head>
-        <title>Ewa Warzyńska Smart Design</title>
+        <title>Ewa Warzyńska smartDesign</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Ewa Warzyńska Smart Design
+          Ewa Warzyńska smartDesign
         </h1>
 
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
